2615380335472ba9c547bc30d39e878be3013b20
liumingming <563903721@qq.com>
2014-06-16 11:49:49 +0800

    modify test4.php

--- test4.php
+++ test4.php
@@ -1,11 +1,16 @@
 <?php
 $arr = array (
 		'push_channel' => array (
-				'baidu_greencar_driver' => array (
-						'apiKey' => '4UKLihlZCBECv52aFm4lHRRl',
-						'secretKey' => 'nw7LEx2qeCASrZCDmedmvEGZYZRNmu8l' 
+			'baidu_greencar_driver' => array (
+				'apiKey' => '4UKLihlZCBECv52aFm4lHRRl',
+				'secretKey' => 'nw7LEx2qeCASrZCDmedmvEGZYZRNmu8l' 
 				) 
-		) 
-);
-var_dump($arr);
+			) 
+	     );
+foreach( $arr as $key=>$val){
+    echo 'key= '.$key;
+    if(is_array($val)){
+        var_dump($val);
+    }
+}
 
